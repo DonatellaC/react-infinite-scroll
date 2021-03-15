aa4265930eb77113b92f27fed476b8a6fabb0061
DonatellaC <donatella.carta@mailbox.org>
2021-03-15 17:26:12 +0100

    add: infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [item, setItem] = useState(1);
+  const itemRef = useRef(null);
 
-  const getPhotos = (page) => {
+  const getPhotos = () => {
+    setIsLoading(true);
+
+    //GET
     axios
-      .get(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=10`)
+      .get(
+        `https://jsonplaceholder.typicode.com/photos?_page=${item}&_limit=10`
+      )
       .then((response) => {
-        setPhotos(
-          [...photos, ...response.data]
-        );
-      })
-      .catch((error) => {
-        console.log(error);
+        const newArr = photos.concat(response.data);
+        setPhotos(newArr);
+        setItem((item) => item + 1);
+        setIsLoading(false);
       });
   };
 
@@ -22,14 +28,34 @@ function App() {
     getPhotos();
   }, []);
 
+  const onIntersect = ([entry], observer) => {
+    if (entry.isIntersecting && !isLoading) {
+      observer.unobserve(entry.target);
+      getPhotos();
+      observer.observe(itemRef.current);
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading) {
+      const observer = new IntersectionObserver(onIntersect, { threshold: 1 });
+      if (itemRef.current) observer.observe(itemRef.current);
+      return () => observer && observer.disconnect();
+    }
+  }, [isLoading]);
 
   return (
-    <div className="row">
-      {photos.map((photo) => (
-        <div className="column" key={photo.id}>
-          <img src={`${photo.url}`} alt="placeholder" />
-        </div>
-      ))}
+    <div>
+      <div className="row">
+        {photos.map((item) => {
+          return (
+            <div className="column" key={item.id}>
+              <img src={item.url} alt="random color selection" />
+            </div>
+          );
+        })}
+      </div>
+      <div ref={itemRef}>{isLoading && "Loading..."}</div>
     </div>
   );
 }
